e1738ca1faf2b753826f6b82124bf00ec830449a
darkaswht <sebastianolo748@gmail.com>
2026-02-23 08:31:43 -0400

    contenidos del 1 al 10

---
+++ Unidad1/practica_introductoria/javas/7-listas.js
@@ -0,0 +1,7 @@
+//definicion de arrays
+const ciudades = ["Sucre", "La Paz", "Santa Cruz", "Beni", "Pando", "Oruro"];
+console.log(ciudades);
+
+//definir un array abreviadamente
+const paises = ["Bolivia", "Ecuador", "Brasil"];
+console.log(paises);
